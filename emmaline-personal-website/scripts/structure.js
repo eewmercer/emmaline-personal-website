a951b0747184578cd5b7bf73748993aec2eec45d
Emmaline Mercer <eewmercer@gmail.com>
2024-07-05 16:00:43 -0400

    Add about.html page and update flexbox

--- emmaline-personal-website/scripts/structure.js
+++ emmaline-personal-website/scripts/structure.js
@@ -5,31 +5,45 @@ let btnPython = document.querySelector("#btnPython");
 let btnJS = document.querySelector("#btnJS");
 let links = document.querySelector("#gitHubLink");
 
-function addJavaGitHubLinks() {
+let home = document.querySelector("#home");
+let about = document.querySelector("#about");
+// let projects = document.querySelector("#projects");
+
+home.addEventListener("click", () => {
+    window.location.href = "./index.html";
+    console.log('hi');
+});
+
+about.addEventListener("click", () => {
+    window.location.href = "./about.html";
+    console.log('hi');
+});
+
+// projects.addEventListener("click", () => {
+//     window.location.href = "./projects.html";
+//     console.log('hi');
+// });
+
+btnJava.addEventListener("click", () => {
     links.innerHTML = "<a href='https://github.com/eewmercer/dorm_room_arranger' target='_blank'>Dorm Room Arranger App</a>";
-}
+});
 
-function addCSSGitHubLinks() {
+btnCSS.addEventListener("click", () => {
     links.innerHTML = "<a href='https://github.com/eewmercer/emmaline-personal-website' target='_blank'>Personal Website</a>" 
     + "<br />" + "<br />" + "<a href='https://github.com/eewmercer/blockchain-wafflehouse' target='_blank'>Attendance App Using Blockhain</a>";
-}
+});
 
-function addHTMLGitHubLinks() {
-    links.innerHTML = "<a href='https://github.com/eewmercer/emmaline-personal-website' target='_blank'>Personal Website</a>";
-}
+btnHTML.addEventListener("click", () => {
+    links.innerHTML = "<a href='https://github.com/eewmercer/emmaline-personal-website' target='_blank'>Personal Website</a>"
+    + "<br />" + "<br />" + "<a href='https://github.com/eewmercer/blockchain-wafflehouse' target='_blank'>Attendance App Using Blockhain</a>";
+});
 
-function addPythonGitHubLinks() {
+btnPython.addEventListener("click", () => {
     links.innerHTML = "<a href='https://github.com/eewmercer/CS-120---Final-Project/tree/main' target='_blank'>Choose Your Own Adventure Game</a>";
+});
 
-}
-
-function addJSGitHubLinks() {
+btnJS.addEventListener("click", () => {
     links.innerHTML = "<a href='https://github.com/eewmercer/emmaline-personal-website' target='_blank'>Personal Website</a>" 
     + "<br />" + "<br />" + "<a href='https://github.com/eewmercer/blockchain-wafflehouse' target='_blank'>Attendance App Using Blockhain</a>";
-}
+});
 
-btnJava.addEventListener("click", addJavaGitHubLinks);
-btnCSS.addEventListener("click", addCSSGitHubLinks);
-btnHTML.addEventListener("click", addHTMLGitHubLinks);
-btnPython.addEventListener("click", addPythonGitHubLinks);
-btnJS.addEventListener("click", addJSGitHubLinks);
